93cb19a4fbc7242749a40822d1702d5e41471ec6
Peng Xue Vang <pxvang33@gmail.com>
2019-02-11 15:57:28 -0600

    added add player input field and button

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
-  <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in" />
-  </div>
-);
+class UserPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerName: '',
+    } // end state
+  } // end constructor
+
+  playerNameChange = (event) => {
+    this.setState ({
+      playerName: event.target.value,
+    })
+    console.log('IN STATE', this.state);
+    
+  } // end playerNameChange
+  render() {
+    return(
+      <div>
+        {JSON.stringify(this.props)}
+
+        <h1 id="welcome">
+          Welcome, {this.props.reduxStore.user.username}!
+        </h1> 
+          <input onChange={this.playerNameChange} placeholder="Add Player" />
+          <button >Add Player</button>
+        {/* <LogOutButton className="log-in" /> */}
+      </div>
+    ) // end return
+  } // ends render
+} // ends component
+
 
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({user}) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
+const mapStateToProps = reduxStore => ({
+  reduxStore
 });
 
 // this allows us to use <App /> in index.js
